d94ca8a9bfe798c06379ebcafd07f65337d30883
Mario Gomez <mariogomezarr@gmail.com>
2021-03-17 21:31:23 -0500

    Checkpoint v1.0
    A friendly checkpoint before going forward to v2.0

--- closestpoint.py
+++ closestpoint.py
@@ -14,6 +14,7 @@ Un outil pour trouver la distance la plus courte entre un ensemble de données d
 import pandas as pd
 import scipy.spatial
 import numpy as np
+from tqdm.notebook import tqdm
 
 def upload_xy_data(str):
 
@@ -24,7 +25,7 @@ def upload_xy_data(str):
     """
 
     temp_xy_data = pd.read_csv(str)
-    xy_data = temp_xy_data[['id','X','Y']]
+    xy_data = temp_xy_data[['id','x','y']]
 
     return xy_data
 
@@ -38,33 +39,52 @@ def upload_target_data(str):
     """
 
     temp_target_data = pd.read_csv(str)
-    target_data = temp_target_data[['id', 'X', 'Y', 'category']]
+    target_data = temp_target_data[['id', 'x', 'y', 'category']]
 
     return target_data
 
 
-def find_closest_points(dataset1, dataset2, output_unite = 'kilometers'):
+def find_closest_points(dataset1, dataset2, output_unit = None):
 
     """ Function that relates xy_data and target_data and finds the closest point between the first dataframe and the categories of the second one.
 
     :param dataset1: This is the static point dataframe, in terms of id, x, and y.
     :param dataset2: This is the target point dataframe, in terms of id, x, y, and category.
-    :param output_unit: Defines the unit of measure in which the shortest distance will be calculated. Kilometers by default. Later versions: meters, degrees.
+    :param output_unit: Defines the unit of measure in which the shortest distance will be calculated. Degrees by default. Later versions: meters, degrees.
 
     """
     
     all_names = dataset2['category'].unique()
-    
-    for i in all_names:
+    tqdm().pandas()
+
+    for i in tqdm(all_names):
+
         type_i = dataset2[dataset2['category'].isin([i])]
-        nearest_point_i = scipy.spatial.distance.cdist(dataset1[['X','Y']], 
-                                type_i[['X','Y']], metric = 'euclidean')
-        df = pd.DataFrame(nearest_point_i, index = dataset1['id'], columns = type_i['id'])
+
+        nearest_point_i = scipy.spatial.distance.cdist(
+            dataset1[['x','y']],
+            type_i[['x','y']],
+            metric = 'euclidean'
+        )
+        
+        df = pd.DataFrame(
+            nearest_point_i,
+            index = dataset1['id'],
+            columns = type_i['id']
+        )
+        
         df2 = df.min(axis = 1)
         df2.name = i
-        df2 = df2/0.009
+        if output_unit == 'k':
+            df2 = df2/0.009
+            title_i = 'nearest_' + i + "_km"
+        elif output_unit == 'm':
+            df2 = df2/0.000009
+            title_i = 'nearest_' + i + "_m"
+        else:
+            title_i = 'nearest_' + i + "_deg"
+
         dataset1[i] = df2.values
-        title_i = 'nearest_' + i
         dataset1.rename(columns = {i: title_i}, inplace = True)
 
     return dataset1
